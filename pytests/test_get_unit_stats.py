9647bddf73ea18a7d9cf5d637ea08ba9fcb320f8
MarTrepodi <martrepodi@gmail.com>
2023-05-16 19:47:26 -0400

    chore: correct test case class name in test_get_unit_stats.py

--- pytests/test_get_unit_stats.py
+++ pytests/test_get_unit_stats.py
@@ -2,14 +2,14 @@ from unittest import TestCase, main
 from swgoh_comlink import SwgohComlink
 
 
-class TestGetPlayer(TestCase):
+class TestGetUnitStats(TestCase):
     def test_get_unit_stats(self):
         """
         Test that player data can be retrieved from game server correctly
         """
         comlink = SwgohComlink()
-        allyCode = 245866537
-        p = comlink.get_player(allycode=allyCode)
+        ally_code = 245866537
+        p = comlink.get_player(allycode=ally_code)
         unit_stats = comlink.get_unit_stats(p['rosterUnit'], flags=['calcGP', 'gameStyle'])
         self.assertTrue('gp' in unit_stats['stats'].keys())
 
