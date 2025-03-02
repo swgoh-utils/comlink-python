7616b0c4958f51eac88a20f0024a177e98d4c235
MarTrepodi <martrepodi@gmail.com>
2025-03-02 15:54:40 -0500

    refactor: simplify code and improve consistency
    Updated variable assignments and formatting to enhance readability and maintain consistent styling. Transitioned docstring format to Google style for improved developer clarity and standardized descriptions across methods.

--- src/swgoh_comlink/__init__.py
+++ src/swgoh_comlink/__init__.py
@@ -2,11 +2,11 @@ from __future__ import annotations
 
 import sys
 
-if sys.version_info[:2] < (3, 9):
-    exit("Python 3.9 or higher is required for this version of the swgoh-comlink package.")
+if sys.version_info[:2] < (3, 10):
+    exit("Python 3.10 or higher is required for this version of the swgoh-comlink package.")
 
-from .version import __version__ as version
-from .swgoh_comlink import SwgohComlink
+from swgoh_comlink.version import __version__ as version
+from swgoh_comlink.swgoh_comlink import SwgohComlink
 
 __all__ = [
     'SwgohComlink',
