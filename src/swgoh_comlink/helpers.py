7eed7c177035ea21d4a95a6777fd2c05f52419c5
MarTrepodi <martrepodi@gmail.com>
2025-07-23 07:04:32 -0400

    feat(helpers): add function to calculate arena payout time
    Introduced `get_arena_payout` to compute the next arena payout time based on offset and type (fleet or squad). Includes timezone adjustment and ensures the calculated time is always in the future. Added detailed docstring for parameter and return value explanation.

--- src/swgoh_comlink/helpers.py
+++ src/swgoh_comlink/helpers.py
@@ -2,6 +2,7 @@
 """
 Helper objects and functions for swgoh_comlink
 """
+from datetime import datetime, timedelta
 from enum import IntFlag
 from math import floor
 
@@ -260,3 +261,32 @@ def get_max_rank_jump(current_rank: int) -> int:
         return current_rank - (3 + max(floor((current_rank - 1) / 6), 1))
     else:
         return int(round(current_rank * 0.85 - 1))
+
+
+def get_arena_payout(offset: int, fleet: bool = False) -> datetime:
+    """
+    Calculate the next arena payout time.
+
+    This function computes the next payout time based on the given offset, considering
+    whether the requested payout is for fleet or squad arena. It adjusts for time zone
+    offsets and ensures that the computed payout time is always in the future relative
+    to the current time.
+
+    Args:
+        offset (int): Time offset in minutes to adjust the payout time.
+        fleet (bool): Indicates if the payout is for fleet arena (True) or regular arena
+            (False). Defaults to False.
+
+    Returns:
+        datetime: The computed next payout time as a datetime object.
+    """
+    payout = datetime.now()
+    local_offset = -(payout.astimezone().utcoffset().total_seconds() / 60)
+    if fleet:
+        payout = payout.replace(hour=19, minute=0, second=0, microsecond=0)
+    else:
+        payout = payout.replace(hour=18, minute=0, second=0, microsecond=0)
+    payout = payout - timedelta(minutes=(offset + local_offset))
+    if payout < datetime.now():
+        payout = payout + timedelta(days=1)
+    return payout
